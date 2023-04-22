e2e43a79dca3162509752f4b414838cbaab6973b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-22 19:45:02 +0200

    Updated DLLs.

--- Main/Program.cs
+++ Main/Program.cs
@@ -12,5 +12,6 @@ for (int i = 0; i < array.Length; i++)
 
 Console.WriteLine("Number of elements: " + array.Length);
 Console.WriteLine(array);
+Console.WriteLine("Number of elements 2: " + array2.Length);
 array.CopyTo(ref array2, 10);
 Console.WriteLine(array2);
