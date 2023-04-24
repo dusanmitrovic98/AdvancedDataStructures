cef88e0904ba6e7bcf74555d8ac0be32d99a73d9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-24 15:05:47 +0200

    Updated test script.

--- Main/Program.cs
+++ Main/Program.cs
@@ -16,5 +16,5 @@ Console.WriteLine(array);
 
 var array2 = array.Copy(10);
 Console.WriteLine("Number of elements 2: " + array2.Length);
+Console.WriteLine("Index: " + array2.IndexOf(256));
 Console.WriteLine(array2);
-Console.WriteLine(array2.IndexOf(100));
