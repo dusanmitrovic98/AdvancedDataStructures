255a2ca040f0318e4e72183c4f6b2fa09918e36f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-24 19:28:35 +0200

    Updated DLLs.

--- Main/Program.cs
+++ Main/Program.cs
@@ -8,14 +8,6 @@ var array = new Array<int>(20);
 // {
 //     array[i] = i * i;
 // }
-// array.Add(22);
 
 Console.WriteLine("Number of elements: " + array.Length);
 Console.WriteLine(array);
-
-// * ------------------------------------------------------------------
-
-var array2 = array.Copy(10);
-Console.WriteLine("Number of elements 2: " + array2.Length);
-Console.WriteLine("Index: " + array2.IndexOf(256));
-Console.WriteLine(array2);
