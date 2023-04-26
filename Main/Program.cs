db7ee9f79fe6b54f9245322f568c02af7eef335a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-26 20:50:32 +0200

    Updated scripts.

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,11 +4,5 @@
 // Static arrays
 var array = new Array<int>(20);
 
-for (int i = 19; i < 0; i++)
-{
-    array[i] = i * i;
-}
-
 Console.WriteLine("Number of elements: " + array.Length);
-array.Sort();
 Console.WriteLine(array);
