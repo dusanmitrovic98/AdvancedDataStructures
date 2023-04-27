0fa23dbeb20acd0e084810ebb133e8cce27ec330
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-27 23:18:56 +0200

    Updated Program.cs file.

--- Main/Program.cs
+++ Main/Program.cs
@@ -33,6 +33,7 @@ bool containsThree = dynamicArray.Contains(3); // containsThree = true
 bool containsFour = dynamicArray.Contains(4); // containsFour = false
 
 Console.WriteLine("Contains Three: " + containsThree);
+Console.WriteLine("Contains Four: " + containsFour);
 
 // Get the index of an element in the dynamic array
 int index = dynamicArray.IndexOf(5); // index = 1
